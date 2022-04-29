6cc9dd12a0149a17985381a45d33803bcbb54c6a
abaysevi <abaysevi012@gmail.com>
2022-04-29 07:40:37 +0530

    yoyoyo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:page_transition/page_transition.dart';
+// import 'package:page_transition/page_transition.dart';
 
 import "notifi.dart";
 import "home.dart";
